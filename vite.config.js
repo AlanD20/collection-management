c7dede58af03fa214767f625673fa481ed7b673e
AlanD20 <aland20@pm.me>
2022-08-06 14:19:04 +0200

    change default, configure ts files

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,15 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/ts/app.ts'],
+      refresh: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': ['resources/ts'],
+    },
+  },
 });
