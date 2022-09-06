0d05e85a757a01dd8b15a8056a3cdf4213c0d743
AlanD20 <aland20@pm.me>
2022-09-06 17:31:51 +0200

    - Enabled middlewares

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -31,6 +31,14 @@ class AuthenticatedSessionController extends Controller
     {
         $request->authenticate();
 
+        if (auth()->user()->detail->block) {
+            Auth::guard('web')->logout();
+            $request->session()->invalidate();
+            $request->session()->regenerateToken();
+
+            return redirect()->route('login')->with('error', 'Account is blocked!');
+        }
+
         $request->session()->regenerate();
 
         return redirect()->intended(RouteServiceProvider::HOME);
--- routes/users.php
+++ routes/users.php
@@ -7,35 +7,35 @@ use App\Http\Controllers\User\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/u/{uname}')
-  ->name('u.')
-  // ->middleware(['auth', 'isBlocked'])
-  ->group(function () {
-      Route::get('/', [UserController::class, 'show'])
-      ->name('show');
+    ->name('u.')
+    ->middleware(['auth', 'isBlocked'])
+    ->group(function () {
+        Route::get('/', [UserController::class, 'show'])
+            ->name('show');
 
-      Route::get('/edit', [UserController::class, 'edit'])
-      ->name('edit');
+        Route::get('/edit', [UserController::class, 'edit'])
+            ->name('edit');
 
-      Route::get('/edit/password', [UserController::class, 'edit_password'])
-      ->name('edit.password');
+        Route::get('/edit/password', [UserController::class, 'edit_password'])
+            ->name('edit.password');
 
-      Route::patch('/', [UserController::class, 'update'])
-      ->name('update');
+        Route::patch('/', [UserController::class, 'update'])
+            ->name('update');
 
-      Route::patch('/password', [UserController::class, 'update_password'])
-      ->name('update.password');
+        Route::patch('/password', [UserController::class, 'update_password'])
+            ->name('update.password');
 
-      Route::delete('/', [UserController::class, 'destroy'])
-      ->name('destroy');
+        Route::delete('/', [UserController::class, 'destroy'])
+            ->name('destroy');
 
-      Route::resources([
-          '/collections' => CollectionController::class,
-          '/collections.items' => ItemController::class,
-      ]);
+        Route::resources([
+            '/collections' => CollectionController::class,
+            '/collections.items' => ItemController::class,
+        ]);
 
-      Route::resource('/collections.items.comments', CommentController::class)
-      ->only('store', 'destroy');
+        Route::resource('/collections.items.comments', CommentController::class)
+            ->only('store', 'destroy');
 
-      Route::get('/collections/{collection}/items/{item}/likes', [ItemController::class, 'like'])
-      ->name('collections.items.likes');
-  });
+        Route::get('/collections/{collection}/items/{item}/likes', [ItemController::class, 'like'])
+            ->name('collections.items.likes');
+    });
